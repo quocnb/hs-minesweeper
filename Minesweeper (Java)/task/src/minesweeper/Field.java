724c8378006b49e3547192b5a08936d2b088fc87
Quoc Nguyen <quocnb.vnu@gmail.com>
2024-04-11 14:31:15 +0900

    Stage 4/5: Prepare for battle

--- Minesweeper (Java)/task/src/minesweeper/Field.java
+++ Minesweeper (Java)/task/src/minesweeper/Field.java
@@ -1,16 +1,23 @@
 package minesweeper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Field {
     final String SAFE = ".";
     final String MINE = "X";
+    final String GUEST = "*";
     final int FIELD_SIZE = 9;
-    private final String[][] field;
+    private final int[][] field;
+
+    private List<Integer> guest;
+    private List<Integer> mines;
 
     public Field() {
-        field = new String[FIELD_SIZE][FIELD_SIZE];
+        mines = new ArrayList<>();
+        guest = new ArrayList<>();
+        field = new int[FIELD_SIZE][FIELD_SIZE];
     }
 
     public int getSize() {
@@ -18,30 +25,79 @@ public class Field {
     }
 
     public void print() {
+        System.out.println(" |123456789|");
+        System.out.println("-|---------|");
         for (int i = 0; i < field.length; i++) {
+            System.out.printf("%d|", i + 1);
             for (int j = 0; j < field[0].length; j++) {
-                if (field[i][j] == null) {
-                    System.out.print(SAFE);
-                } else {
-                    System.out.print(field[i][j]);
-                }
+                String text = switch (field[i][j]) {
+                    case -1 -> GUEST;
+                    case 0, 9 -> SAFE;
+                    default -> "" + field[i][j];
+                };
+                System.out.print(text);
             }
+            System.out.print("|");
             System.out.println();
         }
+        System.out.println("-|---------|");
     }
 
     public void resetMines() {
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[0].length; j++) {
-                field[i][j] = null;
+                field[i][j] = 0;
             }
         }
     }
 
-    public void setMines(Map<Integer, List<Integer>> mines) {
-        for (int row : mines.keySet()) {
-            for (int column : mines.get(row)) {
-                field[row][column] = MINE;
+    /**
+     * Set mines to field
+     * @param mines position of mine (= row * numberOfColumn + column)
+     */
+    public void setMines(List<Integer> mines) {
+        for (int minePosition: mines) {
+            int row = minePosition / getSize();
+            int column = minePosition % getSize();
+            field[row][column] = 9;
+            updateStepAroundMines(row, column);
+        }
+        this.mines = mines;
+    }
+
+    /**
+     * Guest mine by position
+     * @param position = row * numberOfColumn + column
+     * @return true: finish game
+     */
+    public boolean guestMine(int row, int column) {
+        if (field[row][column] > 0 && field[row][column] < 9) {
+            System.out.println("There is a number here!");
+            return false;
+        }
+        int position = row * getSize() + column;
+        if (guest.contains(position)) {
+            guest.remove((Integer)position);
+            field[row][column] = 0;
+        } else {
+            guest.add(position);
+            field[row][column] = -1;
+        }
+        if (guest.size() != mines.size()) {
+            return false;
+        }
+        for (int num : guest) {
+            if (!mines.contains(num)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void updateStepAroundMines(int row, int column) {
+        for (int i = Math.max(0, row - 1); i <= Math.min(field.length - 1, row + 1); i++) {
+            for (int j = Math.max(0, column - 1); j <= Math.min(field.length - 1, column + 1); j++) {
+                field[i][j] = Math.min(9, field[i][j] + 1);
             }
         }
     }
--- Minesweeper (Java)/task/src/minesweeper/Main.java
+++ Minesweeper (Java)/task/src/minesweeper/Main.java
@@ -8,24 +8,33 @@ public class Main {
         // write your code here
         Field field = new Field();
         Scanner scanner = new Scanner(System.in);
-        System.out.print("How many mines do you want on the field?");
+
+        // Setup field
+        System.out.print("How many mines do you want on the field? ");
         int numberOfMines = Integer.parseInt(scanner.nextLine());
-        Map<Integer, List<Integer>> mines = new HashMap<>();
+        List<Integer> mines = new ArrayList<>();
+        int totalSpace = field.getSize() * field.getSize();
         while (numberOfMines > 0) {
-            int row = new Random().nextInt(field.getSize());
-            int column = new Random().nextInt(field.getSize());
-            List<Integer> minesInRow = mines.get(row);
-            if (minesInRow == null) {
-                minesInRow = new ArrayList<>();
-            }
-            if (minesInRow.contains(column)) {
+            int index = new Random().nextInt(totalSpace);
+            if (mines.contains(index)) {
                 continue;
             }
-            minesInRow.add(column);
-            mines.put(row, minesInRow);
+            mines.add(index);
             numberOfMines -= 1;
         }
         field.setMines(mines);
         field.print();
+
+        // Find mines
+        boolean isFinish = false;
+        while (!isFinish) {
+           System.out.print("Set/delete mines marks (x and y coordinates): ");
+           String input = scanner.nextLine();
+           int column = Integer.parseInt(input.split(" ")[0]) - 1;
+           int row = Integer.parseInt(input.split(" ")[1]) - 1;
+           isFinish = field.guestMine(row, column);
+           field.print();
+        }
+        System.out.println("Congratulations! You found all mines!");
     }
 }
